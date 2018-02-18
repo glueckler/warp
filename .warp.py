8cd0e7947ac3e647a628acbf5568039b014a3bd4
Dean Glueckler <glueckler@gmail.com>
2018-02-18 12:01:09 -0800

    ready for processing :)

--- .warp.py
+++ .warp.py
@@ -1,4 +1,6 @@
-import os, sys, re
+import os
+import sys
+import re
 from decimal import *
 
 directory = os.listdir('.')
@@ -8,44 +10,47 @@ directory = list(filter(lambda x: x.find('.', 0, 1) == -1, directory))
 
 print "Non-hidden Files in directory"
 for li in directory:
-  print li
+    print li
 
 print '---------------------------'
 
 for song in directory:
-  print song
+    print song
 
-  checkFile = re.match( r'\d+[AB]\s-\s\d\d.*', song)
-  # if song matches the warp regex ie 3A - 87 - .......
-  if checkFile:
-    info = re.match( r'(\d+[AB])(\s-\s)(\d\d+)( - )(\d*\s*)(.*)(\..*\b)', song)
+    checkFile = re.match(r'\d+[AB]\s-\s\d\d.*', song)
+    # if song matches the warp regex ie 3A - 87 - .......
+    if checkFile:
+        info = re.match(
+            r'(\d+[AB])(\s-\s)(\d\d+)( - )(\d*\s*)(.*)(\..*\b)', song)
 
-    key = info.group(1)
-    key = key.lower()
+        key = info.group(1)
+        key = key.lower()
 
-    bpm = info.group(3)
-    bpm = int(bpm)
-    # fold bpm if it's over 138bpm
-    if (bpm > 138):
-      if (bpm % 2 != 0):
-        bpm = Decimal(bpm)
-      bpm = bpm / 2
+        bpm = info.group(3)
+        bpm = int(bpm)
+        # fold bpm if it's over 138bpm
+        if (bpm > 138):
+            if (bpm % 2 != 0):
+                bpm = Decimal(bpm)
+            bpm = bpm / 2
 
-    name = info.group(6)
+        name = info.group(6)
 
-    extension = info.group(7)
+        extension = info.group(7)
 
-    print key
-    print bpm
-    print name
-    print extension
+        print key
+        print bpm
+        print name
+        print extension
 
-    formatted_name = str(bpm) + ' - ' + key + ' - ' + name + extension
-    print formatted_name
-    print '-' * 10
-  
-    #rename the file
-    os.rename(song, formatted_name)
+        formatted_name = str(bpm) + ' - ' + key + ' - ' + name + extension
+        print formatted_name
+        print '-' * 10
 
-  else:
-    print "File Not Needs Processing, moving on.. "
+        # rename the file
+        os.rename(song, formatted_name)
+
+    else:
+        print "File Not Needs Processing, moving on.. "
+
+
